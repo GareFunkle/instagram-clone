1fc65551df50291ccdd41149f109ef020990ad2b
GarFunkle <vincent.durret@gmail.com>
2022-06-22 18:40:04 +0200

    working on users data state management

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,13 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-
-class MobileSceenLayout extends StatelessWidget {
+class MobileSceenLayout extends StatefulWidget {
   const MobileSceenLayout({Key? key}) : super(key: key);
- 
+
+  @override
+  State<MobileSceenLayout> createState() => _MobileSceenLayoutState();
+}
+
+class _MobileSceenLayoutState extends State<MobileSceenLayout> {
+  String username = "";
+
+  @override
+  void initState() async {
+    super.initState();
+    getUsername();
+  }
+
+  void getUsername() async {
+    FirebaseFirestore.instance.collection('users');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Version Mobile'),
+      body: Center(
+        child: Text('Version Mobile'),
       ),
     );
   }
